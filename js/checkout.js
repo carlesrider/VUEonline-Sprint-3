5e069f75f133f76adc2ad6138289d9fa670f0065
Carles Rider <carles@brinkels.com>
2022-12-16 06:25:52 +0100

    Nivell 1

--- js/checkout.js
+++ js/checkout.js
@@ -1,22 +1,65 @@
 
 // Exercise 6
 function validate() {
+	event.preventDefault();
+
 	var error = 0;
 	// Get the input fields
 	var fName = document.getElementById("fName");
+	var fLastN = document.getElementById("fLastN");
 	var fEmail = document.getElementById("fEmail");
+	var fPassword = document.getElementById("fPassword");
+	var fAddress = document.getElementById("fAddress");
+	var fPhone = document.getElementById("fPhone");
 
 	// Get the error elements
 	var errorName = document.getElementById("errorName");
 	var errorEmail = document.getElementById("errorEmail");  
 	
 	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
+
+	const emailFormat = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+	if(fName.value == "" || fName.value.length < 3 || !fName.value.match('^[a-zA-Z]+$')){
+		fName.classList.add("is-invalid");
 		error++;
+	} else {
+		fName.classList.remove("is-invalid");
 	}
 
-	if(fEmail.value == ""){
+	if(fLastN.value == "" || fLastN.value.length < 3 || !fLastN.value.match('^[a-zA-Z]+$')){
+		fLastN.classList.add("is-invalid");
 		error++;
+	} else {
+		fLastN.classList.remove("is-invalid");
+	}
+
+	if(fEmail.value == "" || fEmail.value.length < 3 || !fEmail.value.match(emailFormat)){
+		fEmail.classList.add("is-invalid");
+		error++;
+	} else {
+		fEmail.classList.remove("is-invalid");
+	}
+	
+	if(fPassword.value == "" || fPassword.value.length < 3 || !fPassword.value.match('^[0-9a-zA-Z]+$')){
+		fPassword.classList.add("is-invalid");
+		error++;
+	} else {
+		fPassword.classList.remove("is-invalid");
+	}
+
+	if(fAddress.value == "" || fAddress.value.length < 3){
+		fAddress.classList.add("is-invalid");
+		error++;
+	} else {
+		fAddress.classList.remove("is-invalid");
+	}
+
+	if(fPhone.value == "" || fPhone.value.length != 9  || !fPhone.value.match('^[0-9]*$')){
+		fPhone.classList.add("is-invalid");
+		error++;
+	} else {
+		fPhone.classList.remove("is-invalid");
 	}
 	 
 	if(error>0){
--- js/shop.js
+++ js/shop.js
@@ -75,32 +75,93 @@ var total = 0;
 function buy(id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
+    // let buyedProduct = products.find((product) => product.id == id);
+    for (let i = 0; i < products.length; i++) { 
+        if(products[i].id == id) {
+            buyedProduct = products[i];
+        }
+    }
+    return cartList.push(buyedProduct);
 }
 
 // Exercise 2
 function cleanCart() {
-
+    cartList.length = 0;
+    return cartList;
 }
 
 // Exercise 3
 function calculateTotal() {
     // Calculate total price of the cart using the "cartList" array
+    total = 0;
+    for (let i = 0; i < cartList.length; i++) { 
+        total += cartList[i].price;
+    }
+    return total;
 }
 
 // Exercise 4
 function generateCart() {
     // Using the "cartlist" array that contains all the items in the shopping cart, 
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+    cart.length = 0;
+    for (let i = 0; i < cartList.length; i++) {
+        let existingProduct = cart.find(product => product.id === cartList[i].id);
+        if (existingProduct) {
+            existingProduct.quantity++;
+            existingProduct.subtotal = existingProduct.price * existingProduct.quantity;
+        } else {
+            let product = cartList[i];
+            product.quantity = 1;
+            product.subtotal = product.price * product.quantity;
+            cart.push(product);
+        }
+           
+    }
+    return cart;
 }
 
 // Exercise 5
 function applyPromotionsCart() {
     // Apply promotions to each item in the array "cart"
+    for (let i = 0; i < cart.length; i++) {
+        // Promotion cooking oil
+        if (cart[i].id == 1 && cart[i].quantity >= 3) {
+            cart[i].price = 10;
+            cart[i].subtotalWithDiscount = cart[i].price * cart[i].quantity;
+        } 
+
+        // Promotion Instant cupcake mixture
+        if (cart[i].id == 3 && cart[i].quantity >= 10) {
+            cart[i].price = cart[i].price * (1/3);
+            cart[i].subtotalWithDiscount = cart[i].price * cart[i].quantity;
+        } 
+    }
+    return cart;
 }
 
 // Exercise 6
 function printCart() {
     // Fill the shopping cart modal manipulating the shopping cart dom
+    generateCart();
+    applyPromotionsCart();
+
+    // Add products to table
+    let productsHTML = '';
+    let priceTotal = 0;
+    for (let i = 0; i < cart.length; i++) {
+        if(cart[i].subtotalWithDiscount > 0) {
+            priceTotal = cart[i].subtotalWithDiscount;
+        } else {
+            priceTotal = cart[i].subtotal;
+        }
+        productsHTML += '<tr><th scope="row">'+cart[i].name+'</th><td>$'+cart[i].price+'</td><td>'+cart[i].quantity+'</td><td>$'+priceTotal+'</td></tr>';
+    }
+    document.getElementById('cart_list').innerHTML = productsHTML;
+
+
+    // Update total cart
+    document.getElementById('total_price').innerHTML = calculateTotal();
 }
 
 
